803ed35fef58251ad8a728d64b9d19a6426df417
willkay5508 <70539838+willkay5508@users.noreply.github.com>
2023-04-25 19:04:58 +0000

    default stuff

--- student.py
+++ student.py
@@ -17,8 +17,8 @@ class Piggy(PiggyParent):
         ''' 
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
-        self.LEFT_SPEED = 80
-        self.RIGHT_SPEED = 80
+        self.LEFT_DEFAULT = 80
+        self.RIGHT_DEFAULT = 80
         self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
