4ff5f21e7c11ae69299d594554ea36c2784a2149
willkay5508 <70539838+willkay5508@users.noreply.github.com>
2023-05-02 19:15:52 +0000

    box

--- student.py
+++ student.py
@@ -138,10 +138,13 @@ class Piggy(PiggyParent):
           self.stop()
           time.sleep(1)
           self.servo(2325)
-          """while True:
+          while True:
             self.fwd()
             self.read_distance()
             if self.read_distance() > 250:
+              self.fwd()
+              time.sleep(1)
+              self.stop()
               self.left()
               time.sleep(0.8)
               self.stop()
@@ -149,7 +152,7 @@ class Piggy(PiggyParent):
               time.sleep(1)
               self.stop()
               return False
-          return False"""
+          return False
           
           
       
